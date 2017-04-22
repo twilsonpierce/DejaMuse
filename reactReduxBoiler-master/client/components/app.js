90bc7f3ddad0b92445b6e3a5ad4d6f09918c76bf
Eric Lau <elau89@gmail.com>
2017-04-22 18:17:59 -0400

    landing page testing

--- reactReduxBoiler-master/client/components/app.js
+++ reactReduxBoiler-master/client/components/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
 import Calendar from '../containers/calendar.jsx';
 import Navbar from '../containers/navbar.jsx';
 
